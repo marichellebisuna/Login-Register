889bb757555d3fc1a2c406afa69ee97e8a0efe3a
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-01-28 10:54:01 +1100

    error handling

--- app.js
+++ app.js
@@ -5,6 +5,25 @@ require('dotenv').config();
 
 const app = express();
 
+app.get('/', async (req, res, next) => {
+  res.send('hello from express.');
+});
+
+//error handler
+app.use(async (req, res, next) => {
+  next(createError.NotFound());
+});
+
+app.use(async (err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Port running on port ${PORT}.`));
