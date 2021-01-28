21fde348b4eb91d0be13cdc8e8c40e1298ff704c
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-01-28 12:02:01 +1100

    Mongodb connection and User model created

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express');
 const morgan = require('morgan');
 const createError = require('http-errors');
 require('dotenv').config();
+require('./helpers/init_mongodb');
 const AuthRoute = require('./Routes/Auth.route');
 
 const app = express();
+app.use(morgan('dev'));
 
 app.get('/', async (req, res, next) => {
   res.send('hello from express.');
