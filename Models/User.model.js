7ecfc63914e69e56be9147ced542f964f0c7cd70
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-01-28 14:09:14 +1100

    Hashing password using bcrypt

--- Models/User.model.js
+++ Models/User.model.js
@@ -1,5 +1,7 @@
+const { NotExtended } = require('http-errors');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 const UserSchema = new Schema({
   email: {
@@ -11,6 +13,17 @@ const UserSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+UserSchema.pre('save', async function (next) {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(this.password, salt);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 const User = mongoose.model('user', UserSchema);
