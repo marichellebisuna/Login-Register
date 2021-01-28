ce0d44754d451ca0acc604a653e301c80e8d27c5
Marichelle Bisuna <marichellebisuna@yahoo.com>
2021-01-28 20:18:45 +1100

    Logout userwith Redis

--- Routes/Auth.route.js
+++ Routes/Auth.route.js
@@ -8,6 +8,7 @@ const {
   signRefreshToken,
   verifyRefreshToken,
 } = require('../helpers/jwt_helper');
+const client = require('../helpers/init_redis');
 
 router.post('/login', async (req, res, next) => {
   try {
@@ -65,7 +66,21 @@ router.post('/refresh-token', async (req, res, next) => {
 });
 
 router.delete('/logout', async (req, res, next) => {
-  res.send('logout route');
+  try {
+    const { refreshToken } = req.body;
+    if (!refreshToken) throw createError.BadRequest();
+    const userId = await verifyRefreshToken(refreshToken);
+    client.DEL(userId, (err, val) => {
+      if (err) {
+        console.log(err.message);
+        throw createError.InternalServerError();
+      }
+      console.log(val);
+      res.sendStatus(204);
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
